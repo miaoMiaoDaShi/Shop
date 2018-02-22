160c1384bf5a81a95247845cdd35c58f728921bf
miaoMiaoDaShi <1340751953@qq.com>
2018-02-22 09:31:09 +0800

    Initial commit

---
+++ app/src/main/java/cn/zcoder/xxp/shop/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package cn.zcoder.xxp.shop.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import cn.zcoder.xxp.shop.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
